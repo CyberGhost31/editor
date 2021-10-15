540028338a25fde23aafdb477cfda23d1b713e13
lweiss618 <91871451+lweiss618@users.noreply.github.com>
2021-10-16 00:56:16 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 
 struct ln_t
 {
-    unsigned *str;
-    unsigned size;
-    unsigned length;
-    struct ln_t *next;
-    struct ln_t *prev;
+    wchar_t * str;
+    size_t size;
+    size_t length;
+    struct ln_t * next;
+    struct ln_t * prev;
 };
 
 typedef struct ln_t line;
@@ -27,10 +27,10 @@ struct ed_st_t
 
 typedef struct ed_st_t editor_state;
 
-line *initln(unsigned *string, unsigned size, unsigned length)
+line *initln(wchar_t *string, size_t size, size_t length)
 {
-    line *root;
-    root = (line *)malloc(sizeof(line));
+    line * root;
+    root = (line*) malloc(sizeof(line));
     root->str = string;
     root->size = size;
     root->length = length;
@@ -39,10 +39,10 @@ line *initln(unsigned *string, unsigned size, unsigned length)
     return root;
 }
 
-line *addln(line *ln, unsigned *string, unsigned size, unsigned length)
+line *addln(line *ln, wchar_t *string, size_t size, size_t length)
 {
     line *temp, *p;
-    temp = (line *)malloc(sizeof(line));
+    temp = (line*) malloc(sizeof(line));
     p = ln->next;
     ln->next = temp;
     temp->str = string;
@@ -68,65 +68,67 @@ line *delln(line *ln)
     return (prev);
 }
 
-unsigned *myfgets(FILE *file, unsigned *size, unsigned *length, int *eof_flag)
+wchar_t fget_wc(FILE *file)
 {
-    int k = 1, i = 0;
-    unsigned *str;
-    str = (unsigned *)malloc(256 * sizeof(unsigned));
     unsigned char b1, b2, b3, b4;
-    unsigned code;
-    while ((b1 = fgetc(file)) != EOF && b1 != '\n')
+    wchar_t code;
+    b1 = fgetc(file);
+    if (b1 <= 0x7F)
     {
-        code = 0;
-        if(b1 <= 0x7F)
-        {
-            code = b1;
-        }
-        else if(b1 >= 0xF8)
-            break;
-        else if(b1 >= 0xF0)
-        {
-            b2 = fgetc(file);
-            b3 = fgetc(file);
-            b4 = fgetc(file);
-            code = ((b1 & 0x07) << 18) | ((b2 & 0x3F) << 12) | ((b3 & 0x3F) << 6) | (b4 & 0x3F);
-        }
-        else if(b1 >= 0xE0)
-        {
-            b2 = fgetc(file);
-            b3 = fgetc(file);
-            code = ((b1 & 0x0F) << 12) | ((b2 & 0x3F) << 6) | (b3 & 0x3F);
-        }
-        else if(b1 >= 0xC0)
-        {
-            b2 = fgetc(file);
-            code = ((b1 & 0x1F) << 6) | (b2 & 0x3F);
-        }
+        code = b1;
+    }
+    else if (b1 >= 0xF8)
+        return -1;
+    else if (b1 >= 0xF0)
+    {
+        b2 = fgetc(file);
+        b3 = fgetc(file);
+        b4 = fgetc(file);
+        code = ((b1 & 0x07) << 18) | ((b2 & 0x3F) << 12) | ((b3 & 0x3F) << 6) | (b4 & 0x3F);
+    }
+    else if (b1 >= 0xE0)
+    {
+        b2 = fgetc(file);
+        b3 = fgetc(file);
+        code = ((b1 & 0x0F) << 12) | ((b2 & 0x3F) << 6) | (b3 & 0x3F);
+    }
+    else if (b1 >= 0xC0)
+    {
+        b2 = fgetc(file);
+        code = ((b1 & 0x1F) << 6) | (b2 & 0x3F);
+    }
+    return code;
+}
 
-        if (i == 256 * k - 1)
-            str = realloc(str, 256 * (++k));
+wchar_t *fget_ws(FILE *file, size_t *size, size_t *length)
+{
+    size_t k = 1, i = 0;
+    wchar_t *str;
+    str = (wchar_t*) malloc(256* sizeof(wchar_t));
+    unsigned code;
+    while ((code = fget_wc(file)) != EOF && code != '\n')
+    {
+        if (i == 256 *k - 1)
+            str = realloc(str, 256 *(++k) *sizeof(wchar_t));
         str[i++] = code;
     }
     str[i] = 0;
     *length = i;
     *size = 256 * k;
-    if (feof(file))
-        *eof_flag = 1;
     return str;
 }
 
 line *readfile(char fname[])
 {
-    FILE *file;
+    FILE * file;
     file = fopen(fname, "r");
     line *root, *current;
     long pos;
-    unsigned *temp;
-    unsigned size, length;
-    int eof_flag = 0;
+    wchar_t * temp;
+    size_t size, length;
     if (file == NULL)
     {
-        temp = (unsigned *)malloc(256 * sizeof(unsigned));
+        temp = (wchar_t*) malloc(256* sizeof(wchar_t));
         temp[0] = 0;
         root = initln(temp, 256, 0);
     }
@@ -137,18 +139,18 @@ line *readfile(char fname[])
         rewind(file);
         if (pos)
         {
-            temp = myfgets(file, &size, &length, &eof_flag);
+            temp = fget_ws(file, &size, &length);
             root = initln(temp, size, length);
             current = root;
-            while (eof_flag != 1)
+            while (!feof(file))
             {
-                temp = myfgets(file, &size, &length, &eof_flag);
+                temp = fget_ws(file, &size, &length);
                 current = addln(current, temp, size, length);
             }
         }
         else
         {
-            temp = (unsigned *)malloc(256 * sizeof(unsigned));
+            temp = (wchar_t*) malloc(256 * sizeof(wchar_t));
             temp[0] = 0;
             root = initln(temp, 256, 0);
         }
@@ -161,30 +163,28 @@ void clrmem(line *root)
 {
     line *current, *next;
     current = root;
-    do
-    {
+    do {
         free(current->str);
         next = current->next;
         free(current);
         current = next;
-
     } while (current != NULL);
 }
 
 void print(WINDOW *win, line *a, int offset)
 {
-    int c = 1;
-    unsigned b[2];
+    size_t c = 1;
+    wchar_t b[2];
     b[1] = 0;
-    for (int i = 0; i < a->length; i++)
+    for (size_t i = 0; i < a->length; i++)
     {
-        if(a->str[i] != '\t')
+        if (a->str[i] != '\t')
         {
             if (offset)
                 offset--;
             else
             {
-		b[0]=a->str[i];
+                b[0] = a->str[i];
                 waddwstr(win, b);
                 c++;
             }
@@ -192,12 +192,13 @@ void print(WINDOW *win, line *a, int offset)
         else
         {
             if (offset >= 4)
-                offset-=4;
+                offset -= 4;
             else
             {
-                for (int j = 0; j < 4 - offset; j++)
-                {    
-                    waddch(win, (unsigned) ' ');
+                for (short j = 0; j < 4 - offset; j++)
+                {
+                    waddch(win, (wchar_t)
+                        ' ');
                     c++;
                 }
             }
@@ -207,33 +208,13 @@ void print(WINDOW *win, line *a, int offset)
     }
 }
 
-void render_text(WINDOW *win, editor_state ed)
-{
-    curs_set(0);
-    for (int i = 1; i < LINES - 2; i++)
-    {
-        wmove(win, i, 1);
-        if (ed.top != NULL)
-        {
-            print(win, ed.top, ed.offset_x);
-            ed.top = ed.top->next;
-        }
-        else
-            waddch(win, '~');
-        waddch(win, '\n');
-        
-    }
-    box(win, 0, 0);
-    wrefresh(win);
-}
-
 void get_virt_x(editor_state *ed)
 {
-    int tabs = 0;
-    int num_of_chars;
+    size_t tabs = 0;
+    size_t num_of_chars;
     for (int i = 0; i < ed->real_x - 1; i++)
         if (ed->current->str[i] == 9) tabs++;
-    num_of_chars =  ed->real_x - tabs + tabs * 4;
+    num_of_chars = ed->real_x - tabs + tabs * 4;
     if (ed->saved_offset_x > ed->offset_x)
         ed->offset_x = ed->saved_offset_x;
     if (num_of_chars - ed->offset_x < 1)
@@ -253,13 +234,32 @@ void get_virt_x(editor_state *ed)
         ed->virt_x = num_of_chars - ed->offset_x;
 }
 
+void render_text(WINDOW *win, editor_state ed)
+{
+    curs_set(0);
+    for (int i = 1; i < LINES - 2; i++)
+    {
+        wmove(win, i, 1);
+        if (ed.top != NULL)
+        {
+            print(win, ed.top, ed.offset_x);
+            ed.top = ed.top->next;
+        }
+        else
+            waddch(win, '~');
+        waddch(win, '\n');
+    }
+    box(win, 0, 0);
+    wrefresh(win);
+}
+
 void render_interface(editor_state ed, int key)
 {
     curs_set(0);
     move(LINES - 1, 1);
     insertln();
     attron(A_REVERSE);
-    printw("real: %d/%d; (%d) = \'%c\'", ed.real_y, ed.real_x, key, key);
+    printw("real: %d/%d; \"%s\"", ed.real_y, ed.real_x, ed.current->str);
     attroff(A_REVERSE);
     refresh();
 }
@@ -276,16 +276,16 @@ void init_editor(editor_state *a, char *fname)
     a->rerender_flag = 1;
 }
 
-void insertChr(editor_state *ed, unsigned chr)
+void insertChr(editor_state *ed, wchar_t chr)
 {
-    unsigned temp1, temp2;
+    size_t temp1, temp2;
     if (ed->current->length - 2 == ed->current->size)
     {
         ed->current->size += 256;
-        realloc(ed->current->str, ed->current->size);
+        ed->current->str = realloc(ed->current->str, ed->current->size);
     }
     temp1 = chr;
-    for (unsigned i = ed->real_x - 1; i <= ed->current->length; i++)
+    for (size_t i = ed->real_x - 1; i <= ed->current->length; i++)
     {
         temp2 = ed->current->str[i];
         ed->current->str[i] = temp1;
@@ -294,29 +294,84 @@ void insertChr(editor_state *ed, unsigned chr)
     ed->current->length++;
 }
 
-void delChr(editor_state *ed, unsigned chr)
+void delChr(editor_state *ed)
 {
     if (ed->real_x <= ed->current->length)
     {
-        for (unsigned i = ed->real_x - 1; i <= ed->current->length; i++)
+        for (size_t i = ed->real_x - 1; i <= ed->current->length; i++)
             ed->current->str[i] = ed->current->str[i + 1];
         ed->current->length--;
     }
 }
 
+/*void enter(editor_state *ed)
+{
+    size_t size = 0, length;
+    wchar_t *temp;
+    
+    length = ed->current->length - ed->real_x + 1;
+    
+    while (length <= size)
+        size += 256;
+    temp = (wchar_t*) malloc(size* sizeof(wchar_t));
+    for (size_t i = 0; i <= length; i++)
+        temp[i] = ed->current->str[ed->real_x + i - 1];
+    ed->current->str[ed->real_x - 1] = 0;
+    ed->current->length = ed->real_x - 1;
+    addln(ed->current, temp, size, length);
+    ed->rerender_flag = 1;
+    ed->current = ed->current->next;
+    ed->real_x = ed->saved_real_x = 1;
+    ed->real_y++;
+    if (ed->virt_y < LINES - 3)
+        ed->virt_y++;
+    else
+        ed->top = ed->top->next;
+    ed->offset_x = ed->saved_offset_x = 0;
+
+}*/
+
+void enter(editor_state *ed)
+{
+    size_t size = 0, length;
+    wchar_t *temp;
+    if (ed->current->length == 0)
+        length = 0;
+    else
+        length = ed->current->length - ed->real_x + 1;
+    do {
+        size += 256;
+    } while (length >= size);
+    temp = (wchar_t*) malloc(size * sizeof(wchar_t));
+    for (size_t i = ed->real_x - 1; i < ed->current->length; i++)
+        temp[i - ed->real_x + 1] = ed->current->str[i];
+    temp[length] = 0;
+    ed->current->str[ed->real_x - 1] = 0;
+    ed->current->length = ed->real_x - 1;
+    addln(ed->current, temp, size, length);
+    ed->rerender_flag = 1;
+    ed->current = ed->current->next;
+    ed->real_x = ed->saved_real_x = 1;
+    ed->real_y++;
+    if (ed->virt_y < LINES - 3)
+        ed->virt_y++;
+    else
+        ed->top = ed->top->next;
+    ed->offset_x = ed->saved_offset_x = 0;
+}
+
+
 void save(editor_state *ed)
 {
-    FILE *file;
+    FILE * file;
     file = fopen(ed->filename, "w");
     line *current;
     current = ed->root;
-    do
-    {
+    do {
         for (int i = 0; i < current->length; i++)
-           fputwc(current->str[i], file);
+            fputwc(current->str[i], file);
         if ((current = current->next) != NULL)
-            fputc('\n', file);
-
+            fputwc((wchar_t)'\n', file);
     } while (current != NULL);
     fclose(file);
 
@@ -324,95 +379,134 @@ void save(editor_state *ed)
 
 void process_key(int key, editor_state *ed)
 {
-        if (key == 27)
-            save(ed);
-        if ((key == KEY_UP) && (ed->current->prev != NULL))
+    if (key == 27)
+        save(ed);
+    if ((key == KEY_UP) && (ed->current->prev != NULL))
+    {
+        ed->real_y--;
+        ed->current = ed->current->prev;
+
+        if (ed->current->length < ed->saved_real_x)
+            ed->real_x = ed->current->length + 1;
+        else
+            ed->real_x = ed->saved_real_x;
+        if (1 < ed->virt_y)
+            ed->virt_y--;
+        else
+        {
+            ed->top = ed->top->prev;
+            ed->rerender_flag = 1;
+        }
+    }
+    else if ((key == KEY_DOWN) && (ed->current->next != NULL))
+    {
+        ed->real_y++;
+        ed->current = ed->current->next;
+        if (ed->current->length < ed->saved_real_x)
+            ed->real_x = ed->current->length + 1;
+        else
+            ed->real_x = ed->saved_real_x;
+        if (ed->virt_y < LINES - 3)
+            ed->virt_y++;
+        else
+        {
+            ed->top = ed->top->next;
+            ed->rerender_flag = 1;
+        }
+    }
+    else if ((key == KEY_LEFT) && (1 < ed->real_x))
+    {
+        ed->real_x--;
+        ed->saved_real_x = ed->real_x;
+        if (ed->virt_x == 1)
+        {
+            ed->offset_x--;
+            ed->saved_offset_x = ed->offset_x;
+            ed->rerender_flag = 1;
+        }
+    }
+    else if ((key == KEY_RIGHT) && (ed->real_x <= ed->current->length))
+    {
+        ed->real_x++;
+        ed->saved_real_x = ed->real_x;
+        if (ed->virt_x == COLS - 2)
+        {
+            ed->offset_x++;
+            ed->saved_offset_x = ed->offset_x;
+            ed->rerender_flag = 1;
+        }
+    }
+    else if ((key == KEY_BACKSPACE || key == 127 || key == '\b'))
+    {
+        if (1 < ed->real_x)
+        {
+            ed->real_x--;
+            ed->saved_real_x = ed->real_x;
+            if (ed->virt_x == 1)
+            {
+                ed->offset_x--;
+                ed->saved_offset_x = ed->offset_x;
+            }
+            delChr(ed);
+        }
+        else if (ed->real_x == 1 && ed->current->prev != NULL)
         {
             ed->real_y--;
+            
+            while (ed->current->length + ed->current->prev->length + 1 >= ed->current->prev->size)
+                ed->current->prev->size += 256;
+            ed->current->prev->str = realloc(ed->current->prev->str, ed->current->prev->size * sizeof(wchar_t));
+            
+            for (size_t i = 0; i < ed->current->length; i++)
+                ed->current->prev->str[ed->current->prev->length + i] = ed->current->str[i];
+            ed->real_x = ed->current->prev->length + 1;
+            ed->current->prev->length += ed->current->length;
+            ed->current->prev->str[ed->current->prev->length] = 0;
             ed->current = ed->current->prev;
-
-            if (ed->current->length < ed->saved_real_x)
-                ed->real_x = ed->current->length + 1;
-            else
-                ed->real_x = ed->saved_real_x;
+            delln(ed->current->next);
             if (1 < ed->virt_y)
                 ed->virt_y--;
             else
-            {
                 ed->top = ed->top->prev;
-                ed->rerender_flag = 1;
-            }
         }
-        else if ((key == KEY_DOWN) && (ed->current->next != NULL))
+        ed->rerender_flag = 1;
+    }
+    else if (key == KEY_DC)
+    {
+        if (ed->real_x <= ed->current->length)
         {
-            ed->real_y++;
-            ed->current = ed->current->next;
-            if (ed->current->length < ed->saved_real_x)
-                ed->real_x = ed->current->length + 1;
-            else
-                ed->real_x = ed->saved_real_x;
-            if (ed->virt_y < LINES - 3)
-                ed->virt_y++;
-            else
-            {
-                ed->top = ed->top->next;
-                ed->rerender_flag = 1;
-            }
+            delChr(ed);
         }
-        else if ((key == KEY_LEFT) && (1 < ed->real_x))
+        else if (ed->real_x == ed->current->length + 1 && ed->current->next != NULL)
         {
-            ed->real_x--;
-            ed->saved_real_x = ed->real_x;
-            if (ed->virt_x == 1)
-            {
-                ed->offset_x--;
-                ed->saved_offset_x = ed->offset_x;
-                ed->rerender_flag = 1;
-            }
+            while (ed->current->length + ed->current->next->length + 1 >= ed->current->size)
+                ed->current->size += 256;
+            ed->current->str = realloc(ed->current->str, ed->current->size * sizeof(wchar_t));
+            for (size_t i = 0; i < ed->current->next->length; i++)
+                ed->current->str[ed->current->length + i] = ed->current->next->str[i];
+            ed->current->length += ed->current->next->length;
+            ed->current->prev->str[ed->current->prev->length] = 0;
+            delln(ed->current->next);
         }
-        else if ((key == KEY_RIGHT) && (ed->real_x <= ed->current->length))
+        ed->rerender_flag = 1;
+    }
+    else if (key == '\n' || key == KEY_ENTER)
+    {
+        enter(ed);
+    }
+    else if (((key >= ' ') || key == '\t') && !(key == KEY_DC || key == KEY_BACKSPACE || key == 127 || key == KEY_UP || key == KEY_DOWN || key == KEY_LEFT || key == KEY_RIGHT))
+    {
+        insertChr(ed, key);
+        ed->real_x++;
+        ed->saved_real_x = ed->real_x;
+        if (ed->virt_x == COLS - 2)
         {
-            ed->real_x++;
-            ed->saved_real_x = ed->real_x;
-            if (ed->virt_x == COLS - 2)
-            {
-                ed->offset_x++;
-                ed->saved_offset_x = ed->offset_x;
-                ed->rerender_flag = 1;
-            }
-
+            ed->offset_x++;
+            ed->saved_offset_x = ed->offset_x;
         }
-        else if ((key == KEY_BACKSPACE || key == 127) && (1 < ed->real_x))
-        {
-            ed->real_x--;
-            ed->saved_real_x = ed->real_x;
-            if (ed->virt_x == 1)
-            {
-                ed->offset_x--;
-                ed->saved_offset_x = ed->offset_x;
-            }
-            delChr(ed, key);
-            ed->rerender_flag = 1;
-
-        }
-        else if (key == KEY_DC)
-        {
-            delChr(ed, key);
-            ed->rerender_flag = 1;
-        }
-        else if (((key >= ' ') || key == '\t') && !(key == KEY_DC || key == KEY_BACKSPACE || key == 127 || key == KEY_UP || key == KEY_DOWN || key == KEY_LEFT || key == KEY_RIGHT))
-        {
-            insertChr(ed, key);
-            ed->real_x++;
-            ed->saved_real_x = ed->real_x;
-            if (ed->virt_x == COLS - 2)
-            {
-                ed->offset_x++;
-                ed->saved_offset_x = ed->offset_x;
-            }
-            ed->rerender_flag = 1;
-        }
-        get_virt_x(ed);
+        ed->rerender_flag = 1;
+    }
+    get_virt_x(ed);
 }
 
 void editor(char *fname)
@@ -430,7 +524,7 @@ void editor(char *fname)
     noecho();
     keypad(stdscr, TRUE);
     set_escdelay(100);
-    int curr_lines = LINES;
+    unsigned curr_lines = LINES;
     int key = 0;
     int rerender_flag = 1;
     while (key != 27)
@@ -441,7 +535,7 @@ void editor(char *fname)
             if (LINES != curr_lines)
             {
                 wresize(win, LINES - 1, COLS);
-                
+
                 if (ed.virt_y > LINES - 3)
                 {
                     while (ed.virt_y > LINES - 3)
@@ -453,14 +547,12 @@ void editor(char *fname)
                 curr_lines = LINES;
             }
             ed.rerender_flag = 1;
-            
         }
         if (ed.rerender_flag)
             render_text(win, ed);
         ed.rerender_flag = 0;
         move(ed.virt_y, ed.virt_x);
         curs_set(1);
-        // key = get_wch();
         get_wch(&key);
         process_key(key, &ed);
     }
@@ -478,7 +570,7 @@ int main(int argc, char *argv[])
     }
     else
     {
-        printf("Usage : editor [filename]\n");
+        printf("Usage : editor[filename], %d\n", EOF);
         exit(1);
     }
     return 0;
